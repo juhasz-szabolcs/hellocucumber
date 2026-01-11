58096e1dcfa96ce80b00a54297aa91dfbc6e277c
Szabolcs Juhász <183376403+juhasz-szabolcs@users.noreply.github.com>
2026-01-12 00:07:04 +0100

    Refactor: IsItFriday osztály áthelyezése src/main/java-ba
    Refactor: Alkalmazás kód áthelyezése a megfelelő mappába
    
    - IsItFriday osztály áthelyezve src/main/java/hellocucumber/-ba
    - Eltávolítva a StepDefinitions.java fájlból
    - Dokumentáció frissítve a változásokkal

--- src/test/java/hellocucumber/StepDefinitions.java
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -5,12 +5,6 @@ import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 import static org.assertj.core.api.Assertions.assertThat;
 
-class IsItFriday {
-    static String isItFriday(String today) {
-        return "Friday".equals(today) ? "TGIF" : "Nope";
-    }
-}
-
 public class StepDefinitions {
     private String today;
     private String actualAnswer;
